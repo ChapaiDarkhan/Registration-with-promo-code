0093cda83f42b77fdc99b22f12faf9a554b67c54
darkhan <chapaid8@gmail.com>
2022-05-26 14:43:50 +0600

    [RGT-7] Add method str to models

--- task_2/core/models.py
+++ task_2/core/models.py
@@ -6,7 +6,13 @@ class User(AbstractUser):
     promo_code = models.CharField(max_length=124, blank=True, null=True)
     bonus = models.IntegerField(default=0, null=True, blank=True)
 
+    def __str__(self):
+        return self.username
+
 
 class InvitationCode(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     invitation_code = models.CharField(max_length=124, blank=True, null=True)
+
+    def __str__(self):
+        return self.user
